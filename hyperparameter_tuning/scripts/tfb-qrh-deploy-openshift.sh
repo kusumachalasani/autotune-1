f0c0deeeceadc3f793e5dd1890155fb1aa747792
kusumachalasani <kchalasa@redhat.com>
2021-09-23 03:20:56 -0400

    TFB benchmark with standalone DB
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- hyperparameter_tuning/scripts/tfb-qrh-deploy-openshift.sh
+++ hyperparameter_tuning/scripts/tfb-qrh-deploy-openshift.sh
@@ -221,8 +221,8 @@ function createInstances() {
 	#Create the deployments and services
 
 	# Deploy one instance of DB
-	oc create -f ${MANIFESTS_DIR}/postgres.yaml -n ${NAMESPACE}
-	sleep 10
+#	oc create -f ${MANIFESTS_DIR}/postgres.yaml -n ${NAMESPACE}
+#	sleep 10
 
 	for(( inst=0; inst<"${SERVER_INSTANCES}"; inst++ ))
 	do
@@ -298,10 +298,19 @@ function createInstances() {
                         fi
 
                 done
-
-                sed -i "/env:/a \ \ \ \ \ \ \ \ \ \ \ \ value: \"${OPTIONS_VAR}\" " ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
+		
+		sed -i "/env:/a \ \ \ \ \ \ \ \ \ \ \ \ value: \"${OPTIONS_VAR}\" " ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
                 sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ - name: "JAVA_OPTIONS"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
 
+		### Update postgres informations - specific to MW hardware runs
+		sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ \ \ value: "jdbc:postgresql://mwperf-server02.perf.lab.eng.rdu2.redhat.com:5432/techempower?loggerLevel=OFF&disableColumnSanitiser=true&assumeMinServerVersion=12&sslmode=disable"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
+		sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ - name: "QUARKUS_DATASOURCE_JDBC_URL"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
+
+		sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ \ \ value: "techempower"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
+		sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ - name: "QUARKUS_DATASOURCE_USERNAME"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
+
+		sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ \ \ value: "techempower"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
+		sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ - name: "QUARKUS_DATASOURCE_PASSWORD"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
 
 
 		oc create -f ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml -n ${NAMESPACE}
