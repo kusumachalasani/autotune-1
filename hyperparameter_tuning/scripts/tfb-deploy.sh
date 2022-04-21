d337d457115e5681feefc007a188a8b075a0a10a
kusumachalasani <kchalasa@redhat.com>
2022-04-22 00:24:49 +0530

    remove gcpolicy
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- hyperparameter_tuning/scripts/tfb-deploy.sh
+++ hyperparameter_tuning/scripts/tfb-deploy.sh
@@ -274,8 +274,8 @@ function createInstances() {
 
 		user_options=$(echo ${OPTIONS_VAR} | tr ";" "\n")
 
-		OPTIONS_VAR=""
-		#OPTIONS_VAR="-server -XX:+UseG1GC -XX:MaxRAMPercentage=70"
+		#OPTIONS_VAR=""
+		OPTIONS_VAR="-server -XX:+UseG1GC -XX:MaxRAMPercentage=70"
 		for useroption in ${user_options}
 		do
 			OPTIONS_VAR="${OPTIONS_VAR} ${useroption}"
