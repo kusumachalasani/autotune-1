5b666aae5057cf443bfb652602c679208d5e092a
kusumachalasani <kchalasa@redhat.com>
2022-04-21 23:14:14 +0530

    add gcpolicy
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- hyperparameter_tuning/tunables.py
+++ hyperparameter_tuning/tunables.py
@@ -41,36 +41,7 @@ def get_all_tunables():
                         '"maximize", "hpo_algo_impl": "optuna_tpe", "tunables": [{"name": "cpuRequest", "value_type": ' \
                         '"double", "upper_bound": 4, "lower_bound": 1, "step": 0.01}, {"name": "memoryRequest", ' \
                         '"value_type": "integer", "upper_bound": 4096, "lower_bound": 270, "step": 1} , {"name": "gcpolicy", ' \
-                        '"value_type": "categorical", "choices": ["UseG1GC", "UseParallelGC", "UseSerialGC", "UseShenandoahGC", "UseZGC"]}, {"name": "quarkusthreadpoolcorethreads", ' \
-                        '"value_type": "integer", "upper_bound": 32, "lower_bound": 1, "step": 1}, {"name": "quarkusthreadpoolqueuesize", ' \
-                        '"value_type": "integer", "upper_bound": 10000, "lower_bound": 0, "step": 10}, {"name": "quarkusdatasourcejdbcminsize", ' \
-                        '"value_type": "integer", "upper_bound": 12, "lower_bound": 1, "step": 1}, {"name": "quarkusdatasourcejdbcmaxsize", ' \
-                        '"value_type": "integer", "upper_bound": 90, "lower_bound": 12, "step": 1}, {"name": "FreqInlineSize", ' \
-                        '"value_type": "integer", "upper_bound": 500, "lower_bound": 325, "step": 1}, {"name": "MaxInlineLevel", ' \
-                        '"value_type": "integer", "upper_bound": 50, "lower_bound": 9, "step": 1}, {"name": "MinInliningThreshold", ' \
-                        '"value_type": "integer", "upper_bound": 200, "lower_bound": 0, "step": 1}, {"name": "CompileThreshold", ' \
-                        '"value_type": "integer", "upper_bound": 10000, "lower_bound": 1000, "step": 10}, {"name": "CompileThresholdScaling", ' \
-                        '"value_type": "double", "upper_bound": 15, "lower_bound": 1, "step": 0.1}, {"name": "ConcGCThreads", ' \
-                        '"value_type": "integer", "upper_bound": 8, "lower_bound": 1, "step": 1}, {"name": "InlineSmallCode", ' \
-                        '"value_type": "integer", "upper_bound": 5000, "lower_bound": 500, "step": 1}, {"name": "LoopUnrollLimit", ' \
-                        '"value_type": "integer", "upper_bound": 250, "lower_bound": 20, "step": 1}, {"name": "LoopUnrollMin", ' \
-                        '"value_type": "integer", "upper_bound": 20, "lower_bound": 0, "step": 1}, {"name": "MinSurvivorRatio", ' \
-                        '"value_type": "integer", "upper_bound": 48, "lower_bound": 3, "step": 1}, {"name": "NewRatio", ' \
-                        '"value_type": "integer", "upper_bound": 10, "lower_bound": 1, "step": 1}, {"name": "TieredStopAtLevel", ' \
-                        '"value_type": "integer", "upper_bound": 4, "lower_bound": 0, "step": 1}, {"name": "TieredCompilation", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "AllowParallelDefineClass", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "AllowVectorizeOnDemand", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "AlwaysCompileLoopMethods", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "AlwaysPreTouch", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "AlwaysTenure", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "BackgroundCompilation", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "DoEscapeAnalysis", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "UseInlineCaches", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "UseLoopPredicate", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "UseStringDeduplication", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "UseSuperWord", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}, {"name": "UseTypeSpeculation", ' \
-                        '"value_type": "categorical", "choices": ["true", "false"]}]} '
+                        '"value_type": "categorical", "choices": ["UseG1GC", "UseParallelGC", "UseSerialGC", "UseShenandoahGC", "UseZGC"]}]} '
 
     search_space = json.loads(search_space_json)
     id = search_space["id"]
